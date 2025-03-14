8d0aa774d161115d2acd38b42cf8ab104c5866be
Rain <rain@oxide.computer>
2025-03-14 04:30:57 +0000

    runpath?
    Created using spr 1.3.6-beta.1

--- .github/buildomat/ci-env.sh
+++ .github/buildomat/ci-env.sh
@@ -7,3 +7,14 @@ export CARGO_TERM_COLOR=always
 
 # Fail on warnings.
 export RUSTFLAGS="-D warnings"
+
+# We only build once, so there's no need to incur the overhead of incremental compilation.
+export CARGO_INCREMENTAL=0
+
+# When running on illumos we need to pass an additional runpath that is
+# usually configured via ".cargo/config" but the `RUSTFLAGS` env variable
+# takes precedence. This path contains oxide specific libraries such as
+# libipcc.
+if [[ $target_os == "illumos" ]]; then
+    RUSTFLAGS="$RUSTFLAGS -C link-arg=-R/usr/platform/oxide/lib/amd64"
+fi
