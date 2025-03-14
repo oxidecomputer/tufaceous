2882da54eb8017cd2a86058d4130decc667b767b
Rain <rain@oxide.computer>
2025-03-14 04:26:08 +0000

    [spr] initial version
    Created using spr 1.3.6-beta.1

---
+++ .github/buildomat/ci-env.sh
@@ -0,0 +1,9 @@
+# Setup shared across Buildomat CI builds.
+#
+# This file contains environment variables shared across Buildomat CI jobs.
+
+# Color the output for easier readability.
+export CARGO_TERM_COLOR=always
+
+# Fail on warnings.
+export RUSTFLAGS="-D warnings"
