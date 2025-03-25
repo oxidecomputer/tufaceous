ad5e0d3189c9de12a811c9e071a5f59b7ab39e6c
Rain <rain@oxide.computer>
2025-03-25 15:44:22 -0700

    [2/n] add tests for duplicate control plane zones and artifacts
    We already have code which ensures that duplicate zones and artifacts are detected. Add tests to exercise these code paths (we're going to hoist them to a higher level in a subsequent commit).
    
    While writing this code, I realized that we don't exit with code 0 when encountering duplicate artifacts. That's not good. I'll fix it in a subsequent commit.
    
    Reviewers: iliana
    
    Reviewed By: iliana
    
    Pull Request: https://github.com/oxidecomputer/tufaceous/pull/18

--- bin/tests/integration-tests/command_tests.rs
+++ bin/tests/integration-tests/command_tests.rs
@@ -223,6 +223,66 @@ fn test_assemble_fake_non_semver() -> Result<()> {
     Ok(())
 }
 
+#[test]
+fn test_assemble_duplicate_zone() -> Result<()> {
+    let log_config = ConfigLogging::File {
+        level: ConfigLoggingLevel::Trace,
+        path: "UNUSED".into(),
+        if_exists: ConfigLoggingIfExists::Fail,
+    };
+    let logctx = LogContext::new("test_assemble_duplicate_zone", &log_config);
+    let tempdir = tempfile::tempdir().unwrap();
+    let key = Key::generate_ed25519()?;
+
+    let archive_path = tempdir.path().join("archive.zip");
+
+    let mut cmd = make_cmd(&key);
+    cmd.args([
+        "assemble",
+        "--skip-all-present",
+        "invalid-manifests/duplicate-zone.toml",
+    ]);
+    cmd.arg(&archive_path);
+    cmd.assert().failure().stderr(predicate::str::contains(
+        "duplicate zones are not allowed \
+         (zone1.tar.gz and zone1.tar.gz have the same checksum)",
+    ));
+
+    logctx.cleanup_successful();
+    Ok(())
+}
+
+#[test]
+fn test_assemble_duplicate_artifact() -> Result<()> {
+    let log_config = ConfigLogging::File {
+        level: ConfigLoggingLevel::Trace,
+        path: "UNUSED".into(),
+        if_exists: ConfigLoggingIfExists::Fail,
+    };
+    let logctx =
+        LogContext::new("test_assemble_duplicate_artifact", &log_config);
+    let tempdir = tempfile::tempdir().unwrap();
+    let key = Key::generate_ed25519()?;
+
+    let archive_path = tempdir.path().join("archive.zip");
+
+    let mut cmd = make_cmd(&key);
+    cmd.args([
+        "assemble",
+        "--skip-all-present",
+        "invalid-manifests/duplicate-artifact.toml",
+    ]);
+    cmd.arg(&archive_path);
+    // TODO: this should be failure, not success!
+    cmd.assert().success().stderr(predicate::str::contains(
+        "a target named gimlet_sp-fake-gimlet-sp-1.0.0.tar.gz \
+         already exists in the repository",
+    ));
+
+    logctx.cleanup_successful();
+    Ok(())
+}
+
 fn make_cmd(key: &Key) -> Command {
     let mut cmd = Command::cargo_bin("tufaceous").unwrap();
     cmd.env("TUFACEOUS_KEY", key.to_string());
