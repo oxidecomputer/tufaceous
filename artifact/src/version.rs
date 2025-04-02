df06d6361ec58c75a48f09266a3dd33ed19f915b
David Pacheco <dap@oxidecomputer.com>
2025-04-02 14:50:55 -0700

    preserve formatting in some Display impls

--- artifact/src/version.rs
+++ artifact/src/version.rs
@@ -106,7 +106,7 @@ impl FromStr for ArtifactVersion {
 
 impl fmt::Display for ArtifactVersion {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(self.as_str())
+        self.as_str().fmt(f)
     }
 }
 
